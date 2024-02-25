1bbbc718de3f72671b6f06fa68cf6fa8b2682073
achraf elabouye <achrafelabouye@gmail.com>
2024-02-25 04:14:11 +0100

    up

--- resources/views/Professeur/index.blade.php
+++ resources/views/Professeur/index.blade.php
@@ -102,7 +102,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                <button type="submit" class="btn btn-primary">Save Response</button>
+                <button type="submit" class="btn btn-primary">Save Response </button>
             </div>
         </div>
     </div>
